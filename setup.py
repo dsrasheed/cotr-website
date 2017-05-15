a605f43149679672ca34cfefe7c72ca8685fd89c
Daniyaal Rasheed <xcub101@gmail.com>
2017-05-15 07:10:29 -0500

    Add dependencies in setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup
 
 setup(
 	name="ColorsOfTheRegion",
-	description=__doc__,
 	packages=["cotr"],
 	include_package_data=True,
 	install_requires=[
-		"flask"
+		"flask",
+		"flask-sqlalchemy",
+		"flask-wtforms",
+		"flask-bcrypt"
 	]
 )
