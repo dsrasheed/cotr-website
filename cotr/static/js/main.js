d324be337f5d10778ca5ec530771f6524c0937f9
Daniyaal Rasheed <xcub101@gmail.com>
2017-05-30 13:15:41 -0500

    Change tabs in static files

--- cotr/static/js/main.js
+++ cotr/static/js/main.js
@@ -1,45 +1,45 @@
 (function() {
-	'use strict';
+    'use strict';
 
-	var nav = document.getElementsByTagName("nav")[0];
-	var scrolled = false;
+    var nav = document.getElementsByTagName("nav")[0];
+    var scrolled = false;
 
-	function onScroll() {
-		var sTop = document.body.scrollTop;
-		
-		if (sTop > 0 && !scrolled) {
-			nav.style.height = "80px";
-			nav.style.backgroundColor = "white";
-			nav.style.color = "black";
-			nav.style.borderBottomWidth= "0";
+    function onScroll() {
+        var sTop = document.body.scrollTop;
+        
+        if (sTop > 0 && !scrolled) {
+            nav.style.height = "80px";
+            nav.style.backgroundColor = "white";
+            nav.style.color = "black";
+            nav.style.borderBottomWidth= "0";
 
-			scrolled = true;
-		} else if (sTop <= 0 && scrolled) {
-			nav.style.height = "130px";
-			nav.style.backgroundColor = "transparent";
-			nav.style.color = "white";
-			nav.style.borderBottomWidth = "1px";
+            scrolled = true;
+        } else if (sTop <= 0 && scrolled) {
+            nav.style.height = "130px";
+            nav.style.backgroundColor = "transparent";
+            nav.style.color = "white";
+            nav.style.borderBottomWidth = "1px";
 
-			scrolled = false;
-		}
-	}
-	
-	var triLeft = document.getElementsByClassName("tri-left")[0];
-	var triRight = document.getElementsByClassName("tri-right")[0];
-	
-	function resizeTriangles() {
-		var width = document.body.clientWidth;
-		var height = document.body.clientHeight;
-		
-		var triWidth = Math.floor(width / 2) + "px";
-		console.log(triWidth);			
-		triLeft.style.borderRightWidth = triWidth;
-		triRight.style.borderLeftWidth = triWidth;
-	}
+            scrolled = false;
+        }
+    }
+    
+    var triLeft = document.getElementsByClassName("tri-left")[0];
+    var triRight = document.getElementsByClassName("tri-right")[0];
+    
+    function resizeTriangles() {
+        var width = document.body.clientWidth;
+        var height = document.body.clientHeight;
+        
+        var triWidth = Math.floor(width / 2) + "px";
+        console.log(triWidth);          
+        triLeft.style.borderRightWidth = triWidth;
+        triRight.style.borderLeftWidth = triWidth;
+    }
 
-	addEventListener("scroll", onScroll);
-	
-	resizeTriangles();
-	addEventListener("resize", resizeTriangles);
-	
+    addEventListener("scroll", onScroll);
+    
+    resizeTriangles();
+    addEventListener("resize", resizeTriangles);
+    
 })();
