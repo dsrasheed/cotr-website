812f404c352fbd9f3d8115c69e08307f698dbd96
Daniyaal Rasheed <xcub101@gmail.com>
2017-06-06 10:37:45 -0500

    Fixed bug - Store ascii hash instead of binary

--- cotr/visitors/models.py
+++ cotr/visitors/models.py
@@ -5,13 +5,13 @@ from cotr import db, bcrypt
 class Visitor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(80), unique=True)
-    token = db.Column(db.String(25))
+    token = db.Column(db.String(60))
 
     def __init__(self, email, token=None):
         self.email = email
         if token is None:
-            token = get_token()
-        self.token = bcrypt.generate_password_hash(token)
+            token = self.get_token()
+        self.token = bcrypt.generate_password_hash(token).decode('ascii')
     
     def get_token(self):
         characters = ascii_lowercase + ascii_uppercase + digits
